167c924ae693df2bab57ba4a378d867b19ab614c
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-02-25 23:26:15 +0100

    variables de entorno

--- core/auth/actions/auth-actions.ts
+++ core/auth/actions/auth-actions.ts
@@ -44,3 +44,5 @@ export const authCheckStatus = async () => {
     return null
   }
 };
+
+
--- core/auth/api/productsApi.ts
+++ core/auth/api/productsApi.ts
@@ -1,8 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
+import { Platform } from "react-native";
+
+const STAGE = process.env.EXPO_PUBLIC_STAGE || "dev";
+
+export const API_URL =
+  STAGE === "prod"
+    ? process.env.EXPO_PUBLIC_API_URL
+    : Platform.OS === "ios"
+    ? process.env.EXPO_PUBLIC_API_URL_IOS
+    : process.env.EXPO_PUBLIC_API_URL_ANDROID;
 
 const productsApi = axios.create({
-    baseURL: "http://localhost:3000/api"
-})
+  baseURL: API_URL,
+});
 
-export { productsApi }
-
+export { productsApi };

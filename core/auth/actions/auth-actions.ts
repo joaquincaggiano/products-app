ee2b6511611ab4255c3f5957f8dee5edd16b43d4
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-02-25 22:56:04 +0100

    acciones auth

---
+++ core/auth/actions/auth-actions.ts
@@ -0,0 +1,46 @@
+import { productsApi } from "../api/productsApi";
+import { User } from "../interfaces/user";
+
+export interface AuthResponse {
+  id: string;
+  email: string;
+  fullName: string;
+  isActive: boolean;
+  roles: string[];
+  token: string;
+}
+
+const returnUserToken = (data: AuthResponse): { user: User; token: string } => {
+  const { token, ...user } = data;
+
+  return {
+    user,
+    token,
+  };
+};
+
+export const authLogin = async (email: string, password: string) => {
+  email = email.toLowerCase();
+
+  try {
+    const { data } = await productsApi.post<AuthResponse>("/auth/login", {
+      email,
+      password,
+    });
+
+    return returnUserToken(data);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Invalid credentials");
+  }
+};
+
+export const authCheckStatus = async () => {
+    try {
+        const { data } = await productsApi.get<AuthResponse>("/auth/check-status");
+        return returnUserToken(data);
+    } catch (error) {
+        console.log(error);
+        throw new Error("Invalid token");
+    }
+}

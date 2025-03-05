2fbb071ac1ede4ad2c41f3382600ee8279893a40
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-03-05 14:34:18 +0100

    instalar la camara de expo y crear pantalla

--- app/camera/index.tsx
+++ app/camera/index.tsx
@@ -1,11 +1,91 @@
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ThemedText } from "@/theme/components/ThemedText";
+import { useThemeColor } from "@/theme/hooks/useThemeColor";
 
 const CameraScreen = () => {
-  return (
-    <View>
-      <Text>CameraScreen</Text>
-    </View>
-  )
-}
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const primaryColor = useThemeColor({}, "primary");
 
-export default CameraScreen
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View
+        style={{
+          ...styles.container,
+          marginHorizontal: 30,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={styles.message}>
+          We need your permission to show the camera and the gallery
+        </Text>
+        <TouchableOpacity
+          onPress={requestPermission}
+          style={{ backgroundColor: primaryColor }}
+          className="rounded-md p-4 w-full justify-center items-center"
+        >
+          <Text className="text-white text-lg font-kanit">
+            Grant permission
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
+
+export default CameraScreen;

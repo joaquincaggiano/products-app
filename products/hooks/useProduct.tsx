923cc84d2e2be7b7b237623053eba0d929c3ea0b
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-03-05 02:25:54 +0100

    invalidando queries

--- products/hooks/useProduct.tsx
+++ products/hooks/useProduct.tsx
@@ -1,10 +1,14 @@
 import { createUpdateProduct } from "@/core/products/actions/create-update-product";
 import { getProductById } from "@/core/products/actions/get-product-by-id";
 import { Product } from "@/core/products/interfaces/product";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
 import { Alert } from "react-native";
 
 export const useProduct = (id: string) => {
+  const queryClient = useQueryClient();
+  const productIdRef = useRef(id); // puede ser new o uuid
+
   const productQuery = useQuery({
     queryKey: ["products", id],
     queryFn: () => getProductById(id),
@@ -12,8 +16,16 @@ export const useProduct = (id: string) => {
   });
 
   const productMutation = useMutation({
-    mutationFn: async (data: Product) => createUpdateProduct(data),
+    mutationFn: async (data: Product) =>
+      createUpdateProduct({
+        ...data,
+        id: productIdRef.current,
+      }),
     onSuccess: (data: Product) => {
+      productIdRef.current = data.id;
+
+      queryClient.invalidateQueries({ queryKey: ["products", "infinite"] });
+      queryClient.invalidateQueries({ queryKey: ["products", id] });
       Alert.alert(
         `Producto guardado`,
         `El producto ${data.title} se ha guardado correctamente`
